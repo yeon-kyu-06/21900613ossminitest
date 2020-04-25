847cc97936aa53ca5b80e11230163f73accc6e19
Lim yeonkyu <21900613@handong.edu>
2020-04-25 14:51:25 +0900

    create loadData function

--- manager.c
+++ manager.c
@@ -55,9 +55,19 @@ void saveData(Product p[], int count){
 int loadData(Product *p){
 	int count=0;
 	FILE*fp;
-
+	
+	fp= fopen("product.txt","rt");
 	//파일 내용을 읽어와서 배열에 값 추가하기
-
+	if(fp==NULL){
+		printf("데이터가 없습니다!\n");
+		return 0;
+	}
+	for(; ; count++){
+		fscanf(fp,"%d %d",&p[count].price,&p[count].weight);
+		fgets(p[count].name,sizeof(p[count].name),fp);
+		if(feof(fp))
+			break;
+	}
 
 
 
